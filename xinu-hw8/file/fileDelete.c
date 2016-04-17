31102c078c2d85af5b40a8051433afe8f4473460
Samuel Merrick Scheel <sscheel@morbius.mscs.mu.edu>
2016-04-16 19:34:41 -0500

    Baby steps meow.

--- xinu-hw8/file/fileDelete.c
+++ xinu-hw8/file/fileDelete.c
@@ -2,10 +2,10 @@
 /* Copyright (C) 2008, Marquette University.  All rights reserved. */
 /*                                                                 */
 /* Modified by                                                     */
-/*                                                                 */
+/* Samuel Scheel                                                   */
 /* and                                                             */
-/*                                                                 */
-/*                                                                 */
+/* Louis Krueger                                                   */
+/* TA-BOT:MAILTO samuel.scheel@marquette.edu louis.krueger@maruqette.edu */
 
 #include <kernel.h>
 #include <memory.h>
@@ -21,5 +21,17 @@ devcall fileDelete(int fd)
     //  and return its space to the free disk block list.
     //  Use the superblock's locks to guarantee mutually exclusive
     //  access to the directory index.
+    if ((fd >= DIRENTRIES) || (NULL == supertab) || (NULL == filetab))
+    {
+        return SYSERR;
+    }
+    wait(supertab->sb_dirlock);
+    if (sbFreeBlock(supertab, fd) == SYSERR)
+    {
+	signal(supertab->sb_dirlock);
+	return SYSERR;
+    }
+    filetab[fd].fn_state = FILE_FREE;
+    signal(supertab->sb_dirlock);
     return OK;
 }
--- xinu-hw8/file/sbFreeBlock.c
+++ xinu-hw8/file/sbFreeBlock.c
@@ -2,10 +2,10 @@
 /* Copyright (C) 2008, Marquette University.  All rights reserved. */
 /*                                                                 */
 /* Modified by                                                     */
-/*                                                                 */
+/* Samuel Scheel                                                   */
 /* and                                                             */
-/*                                                                 */
-/*                                                                 */
+/* Louis Krueger                                                   */
+/* TA-BOT:MAILTO samuel.scheel@marquette.edu louis.krueger@marquette.edu */
 
 #include <kernel.h>
 #include <device.h>
@@ -23,5 +23,30 @@ devcall sbFreeBlock(struct superblock *psuper, int block)
     //  free blocks.  Use the superblock's locks to guarantee
     //  mutually exclusive access to the free list, and write
     //  the changed free list segment(s) back to disk.
+    struct freeblock *freeblk;
+    struct dentry *phw;
+    int diskfd;
+
+    if (NULL == psuper)
+    {
+        return SYSERR;
+    }
+    phw = psuper->sb_disk;
+    if (NULL == phw)
+    {
+        return SYSERR;
+    }
+    diskfd = phw - devtab;
+    wait(supertab->sb_freelock);
+    freeblk = supertab->sb_freelst;
+    while (freeblk->fr_count != FREEBLOCKMAX) 
+    {
+	if (freeblk->fr_next == NULL)
+		break;
+	freeblk = freeblk->fr_next;
+    }
+    seek(diskfd, block);
+    write(diskfd, psuper, sizeof(struct freeblock));
+    signal(supertab->sb_freelock);
     return SYSERR;
 }
